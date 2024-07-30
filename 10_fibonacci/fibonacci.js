611a30720e1503e85de65eb8dec74ff5db14063c
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-30 09:53:52 +0300

    pass 10.11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,23 @@
-const fibonacci = function(n) {
-    if (n <= 0) {
-        throw new Error('The position must be a positive integer.');
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
     }
-    if (n === 1 || n === 2) {
+
+    if (count < 0) {
+        return "OOPS"
+    }
+    if (count === 0) {
+        return 0
+    }
+    if (count === 1 || count === 2) {
         return 1;
     }
 
     let a = 1, b = 1;
-    for (let i = 3; i <= n; i++) {
+    for (let i = 3; i <= count; i++) {
         let temp = a + b;
         a = b;
         b = temp;
