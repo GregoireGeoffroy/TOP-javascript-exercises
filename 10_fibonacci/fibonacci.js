7048cd5746df6c3126912be3d2454d0f523c9c0f
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-30 09:41:28 +0300

    pass 10.1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n <= 0) {
+        throw new Error('The position must be a positive integer.');
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
 
+    let a = 1, b = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
